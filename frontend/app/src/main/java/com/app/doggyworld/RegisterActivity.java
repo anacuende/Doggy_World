89c3bf08c96e34ff706cb02416f57917c61895d0
anacuende <acuendet@fpcoruna.afundacion.org>
2024-05-23 01:44:21 +0200

    Hechas pantallas Login y Register

--- frontend/app/src/main/java/com/app/doggyworld/RegisterActivity.java
+++ frontend/app/src/main/java/com/app/doggyworld/RegisterActivity.java
@@ -20,59 +20,75 @@ import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
-    private EditText usernameEmailEditText;
+    private EditText nameEditText;
+    private EditText usernameEditText;
+    private EditText emailEditText;
     private EditText passwordEditText;
-    private Button loginButton;
+    private EditText confirmPasswordEditText;
     private Button registerButton;
+    private Button loginButton;
     private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
-        usernameEmailEditText = findViewById(R.id.usernameEmailEditText);
+        // Inicializar las vistas
+        nameEditText = findViewById(R.id.nameEditText);
+        usernameEditText = findViewById(R.id.usernameEditText);
+        emailEditText = findViewById(R.id.emailEditText);
         passwordEditText = findViewById(R.id.passwordEditText);
-        loginButton = findViewById(R.id.loginButton);
+        confirmPasswordEditText = findViewById(R.id.confirmPasswordEditText);
         registerButton = findViewById(R.id.registerButton);
+        loginButton = findViewById(R.id.loginButton);
         sharedPreferences = getSharedPreferences("DoggyWorldPrefs", Context.MODE_PRIVATE);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        // Configurar los listeners de los botones
+        registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String usernameEmail = usernameEmailEditText.getText().toString();
+                String name = nameEditText.getText().toString();
+                String username = usernameEditText.getText().toString();
+                String email = emailEditText.getText().toString();
                 String password = passwordEditText.getText().toString();
+                String confirmPassword = confirmPasswordEditText.getText().toString();
 
-                if (usernameEmail.isEmpty() || password.isEmpty()) {
-                    Toast.makeText(LoginActivity.this, "Rellene todos los campos", Toast.LENGTH_SHORT).show();
+                if (name.isEmpty() || username.isEmpty() || email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+                    Toast.makeText(RegisterActivity.this, "Rellene todos los campos", Toast.LENGTH_SHORT).show();
                 } else {
-                    iniciarSesion(usernameEmail, password);
+                    registrarUsuario(name, username, email, password, confirmPassword);
                 }
             }
         });
 
-        registerButton.setOnClickListener(new View.OnClickListener() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Navegación a la actividad de registro
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                // Navegar a LoginActivity
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
     }
 
-    private void iniciarSesion(String usernameEmail, String password) {
-        String url = "http://10.0.2.2:8000/api/doggyWorld/session";
+    private void registrarUsuario(String name, String username, String email, String password, String confirmPassword) {
         JSONObject body = new JSONObject();
         try {
-            body.put("user", usernameEmail);
+            body.put("name", name);
+            body.put("username", username);
+            body.put("email", email);
             body.put("password", password);
+            body.put("confirm_password", confirmPassword);
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            Toast.makeText(RegisterActivity.this, "Error creating JSON request", Toast.LENGTH_SHORT).show();
+            return;
         }
 
+        String url = "http://10.0.2.2:8000/api/doggyWorld/user/register";
         JsonObjectRequest request = new JsonObjectRequest(
                 Request.Method.POST,
                 url,
@@ -83,21 +99,20 @@ public class LoginActivity extends AppCompatActivity {
                         try {
                             // Obtener el mensaje de respuesta del servidor
                             String message = response.getString("message");
-                            // Muestra el mensaje en un Toast
-                            Toast.makeText(LoginActivity.this, message, Toast.LENGTH_SHORT).show();
+                            Toast.makeText(RegisterActivity.this, message, Toast.LENGTH_SHORT).show();
 
+                            // Si el registro fue exitoso, guardar el token y navegar a MainActivity
                             String token = response.getString("token");
                             SharedPreferences.Editor editor = sharedPreferences.edit();
                             editor.putString("token", token);
                             editor.apply();
 
-                            // Navega a MainActivity
-                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
                             startActivity(intent);
                             finish();
                         } catch (Exception e) {
-                            // Si hay un error al procesar la respuesta, muestra un mensaje de error genérico
-                            Toast.makeText(LoginActivity.this, "Error al procesar los datos", Toast.LENGTH_SHORT).show();
+                            // Si hay un error al procesar la respuesta, muestra un mensaje de error específico
+                            Toast.makeText(RegisterActivity.this, "Error al procesar los datos", Toast.LENGTH_SHORT).show();
                         }
                     }
                 },
@@ -106,14 +121,11 @@ public class LoginActivity extends AppCompatActivity {
                     public void onErrorResponse(VolleyError error) {
                         // Maneja el error de la solicitud HTTP
                         if (error.networkResponse == null) {
-                            // Si no hay respuesta del servidor, muestra un mensaje de error interno
-                            Toast.makeText(LoginActivity.this, "Internal Server Error", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(RegisterActivity.this, "Error interno de servidor", Toast.LENGTH_SHORT).show();
                         } else {
-                            // Si hay una respuesta del servidor, obtener el código de estado y el mensaje de error
                             int statusCode = error.networkResponse.statusCode;
                             String errorMessage = new String(error.networkResponse.data);
-                            // Muestra el mensaje de error en un Toast
-                            Toast.makeText(LoginActivity.this, "Error " + statusCode + ": " + errorMessage, Toast.LENGTH_LONG).show();
+                            Toast.makeText(RegisterActivity.this, "Error " + statusCode + ": " + errorMessage, Toast.LENGTH_LONG).show();
                         }
                     }
                 }
